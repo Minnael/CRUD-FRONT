6665018daf65e3196bf9f731b8fe4b85a3261924
Minnael <minnaelcampelo@gmail.com>
2024-02-14 10:53:48 -0300

    primeiro get

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -1,21 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from 'axios'
+
+import './global.css'
 import Forms from "./Components/Forms/Forms.jsx"
 import Tabela from "./Components/Tabela/Tabela.jsx"
-import { useState } from "react";
-import './global.css'
 
 function App() {
-  const dados = [
-    { id: 1, nome: 'Marcos', email: 'marcos@gmail.com', fone: 84987507865 },
-    { id: 2, nome: 'Francis', email: 'francisco@gmail.com', fone: 84987579865 },
-    { id: 3, nome: 'Camila', email: 'camila@gmail.com', fone: 84987304865 },
-  ];
+  const [dadosTabela, setDadosTabela] = useState([]);
 
-  const [dadosTabela, setDadosTabela] = useState(dados);
+  useEffect (() => {
+    axios
+      .get('http://localhost:8800/')
+      .then((response) => setDadosTabela(response.data))
+      .catch((error) => console.log(error))
+      .finally(console.log('dentro do finally'))
+  }, [])
 
   return (  
     <div className="container">
       <Forms />
-      <Tabela dados={dadosTabela}/>
+      {dadosTabela.length > 0 ? (
+        <>
+          <Tabela dados={dadosTabela} />
+        </>
+      ) : (
+        <p>Aguarde, estamos carregando os dados...</p>
+      )}
     </div>
   )
 } 
